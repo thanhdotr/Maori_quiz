3b97e093d76dc289bacc96344f3202b4df5de8aa
thanhdotr <83623687+thanhdotr@users.noreply.github.com>
2022-06-12 09:24:23 +1200

    Update 01_csv_reading.py

--- 01_csv_reading.py
+++ 01_csv_reading.py
@@ -1,14 +1,7 @@
-import pandas as pd
+"""function to read from a csv file using built in csv.
+Created by Thanh Do
+1/06/2022"""
 import csv
-from time import time
-
-def pandas_reading():
-    Word_database = []
-    print("The contents of the above file is as follows:")
-    df = pd.read_csv('Maori Words.csv')
-    # print(df)
-    Word_database = df.values.tolist()
-    print(Word_database)
 
 def csv_reading():
     Word_database = []
@@ -20,12 +13,5 @@ def csv_reading():
         Word_database.append(r)
     c.close()
 
-t0 = time()
-pandas_reading()
-t1 = time()
-csv_reading()
-t2 = time()
 
-print ('function pandas_reading takes %f' %(t1-t0))
-print ('function csv_reading takes %f' %(t2-t1))
 
